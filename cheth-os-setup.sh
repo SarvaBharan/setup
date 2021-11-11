7bed93f5eb27e70f6493b1a68848c8d644b78971
SarvaBharan <chethanraju19@gmail.com>
2021-11-12 01:10:38 +0530

    :zap: keep alive with sudo permission until script finishes

--- cheth-os-setup.sh
+++ cheth-os-setup.sh
@@ -10,6 +10,9 @@ set -euo pipefail
 echo "Setting up your Mac..."
 sudo -v
 
+# Keep-alive: update existing `sudo` time stamp until the script has finished.
+while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
+
 echo "But first, what's your github username?"
 read gitUsername
 
